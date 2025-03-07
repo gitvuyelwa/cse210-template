bbd2ea26d1b243da8463c7e5520e5854778dd93b
gitvuyelwa <vuyi.macanda@gmail.com>
2025-03-07 22:11:35 +0200

    update

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your garde percentage? ");
+        string grade= Console.ReadLine();
+        int gradePercentage = int.Parse(grade);
+        string letter="";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is {letter}.");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("You failed the class.");
+        }
+
+
     }
 }
