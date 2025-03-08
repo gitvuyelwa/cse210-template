16a9dcb95aaf150e9c2c21fe670db7ebde84050b
gitvuyelwa <vuyi.macanda@gmail.com>
2025-03-08 13:49:41 +0200

    update

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int sum = 0;
+        int userNumber = -1;
+
+        while (userNumber !=0)
+        {
+            Console.Write("Enter a number: ");
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            if (userNumber !=0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is {sum}.");
+
+        float average =((float)sum / numbers.Count);
+        Console.WriteLine($"The average is {average}.");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is {max}.");
     }
 }
